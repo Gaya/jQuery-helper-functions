57b406feb236bb9fd0f9e02a7b705e6f6b329872
Gaya Kessler <gaya@twisted.nl>
2012-02-27 15:25:13 +0100

    added another function

--- README.markdown
+++ README.markdown
@@ -2,3 +2,4 @@
 a collection of nice jQuery functions that are not included in the default package.
 ## Currently holds: ##
 - .disableSelection(selector) - disables text & image selection in the element.
+- .makeItWorkForIE(version) - fixes all your problems!
--- jquery-helpers.0.1.js
+++ jquery-helpers.0.1.js
@@ -4,9 +4,11 @@
 
  These functions:
  .disableSelection(selector) - disabled the text / images in the selected element to be selected
+ .makeItWorkForIE(version) - fixes all your problems
 */
 (function($){
 
+//disableSelection
 $.fn.disableSelection = function() {
     return this.each(function() {
         $(this).attr('unselectable', 'on')
@@ -19,6 +21,17 @@ $.fn.disableSelection = function() {
                .each(function() {
                    this.onselectstart = function() { return false; };
                });
+    });
+};
+
+//fix for IE
+$.fn.makeItWorkForIE = function(versions) {
+    return this.each(function() {
+        if ($.browser.msie && parseInt($.browser.version, 10) <= versions) {
+            $(this).css({
+                'display':'none'
+            });
+        }
     });
 };
 
